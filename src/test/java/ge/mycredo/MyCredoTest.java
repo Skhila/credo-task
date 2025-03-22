a6655f20fd7f5f7cb48d6a75925c88b87ffb4371
Skhila <bachanaskhiladze@gmail.com>
2025-03-23 01:45:52 +0400

    Created test class and testng.xml with cross browser functionality

---
+++ src/test/java/ge/mycredo/MyCredoTest.java
@@ -0,0 +1,82 @@
+package ge.mycredo;
+
+import ge.mycredo.data.CredoDataProvider;
+import ge.mycredo.data.Routes;
+import ge.mycredo.steps.LoginPageSteps;
+import ge.mycredo.steps.componentsteps.LanguageChangePopupSteps;
+import ge.mycredo.utils.config.BaseTest;
+import ge.mycredo.utils.config.testlisteners.ScreenshotListener;
+import io.qameta.allure.*;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(ScreenshotListener.class)
+@Epic("Authentication")
+@Feature("Login Functionality")
+public class MyCredoTest extends BaseTest {
+
+    LoginPageSteps loginPageSteps;
+    LanguageChangePopupSteps languageChangePopupSteps;
+
+    @BeforeMethod
+    public void setup() {
+        driver.get(Routes.LOGIN_URL);
+        loginPageSteps = new LoginPageSteps(getDriver(), softAssert, wait);
+        languageChangePopupSteps = new LanguageChangePopupSteps(getDriver(), softAssert, wait);
+    }
+
+    @Test(priority = 1, dataProvider = "negativeLoginDataProvider", dataProviderClass = CredoDataProvider.class)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Verify system behavior with invalid login credentials across different languages")
+    @Story("As a user, I should see appropriate error messages in my selected language when I enter invalid credentials")
+    public void loginWithInvalidCredentialsAcrossDifferentLanguages(String language, String headerText, String userName, String password, String errorText) {
+        loginPageSteps
+                .clickLanguageSwitcherButton();
+
+        languageChangePopupSteps
+                .verifyChangeLanguagePopupHeaderVisibility()
+                .chooseLanguage(language);
+
+        loginPageSteps
+                .verifyHeaderText(headerText)
+                .fillUserName(userName)
+                .fillPassword(password)
+                .clickLoginButton()
+                .verifyErrorText(errorText)
+                .clickErrorCloseButton();
+
+        // ყოველ ტესტ მეთოდში ცალკე ვიძახებ assertAll()-ს რადგან AfterMethod()-ში გამოძახების შემთხვევაში ტესტ მეთოდის ნაცვლად
+        // AfterMethod() ერორდება და კითხვადობას უკარგავს ტესტის შედეგებს
+        softAssert.assertAll();
+    }
+
+    @Test(priority = 2)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Verify system behavior with empty username")
+    @Story("As a user, I should not be able to attempt login with an empty username to maintain security")
+    public void loginWithEmptyUserName() {
+        loginPageSteps
+                .fillUserName("")
+                .fillPassword(RandomStringUtils.randomAlphanumeric(10))
+                .verifyLoginButtonIsDisabled();
+
+        softAssert.assertAll();
+    }
+
+    @Test(priority = 3)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Verify system behavior with empty password")
+    @Story("As a user, I should not be able to attempt login with an empty password to maintain security")
+    public void loginWithEmptyPassword() {
+        loginPageSteps
+                .fillUserName(RandomStringUtils.randomAlphanumeric(10))
+                .fillPassword("")
+                .verifyLoginButtonIsDisabled();
+
+        softAssert.assertAll();
+    }
+
+
+}
